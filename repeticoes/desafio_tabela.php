b951a4c329e374b2fabcd69a9e0304377bf4b191
joaorocha-web <jvrochajf@eaportal.org>
2025-04-10 16:03:40 -0300

    .

--- repeticoes/desafio_tabela.php
+++ repeticoes/desafio_tabela.php
@@ -1,1 +1,18 @@
 <div class="titulo">Desafio Tabela</div>
+
+<?php
+$matriz = [
+    ['01','02','03','04','05'],
+    ['06','07','08','09',10],
+    [11,12,13,14,15],
+    [16,17,18,19,20]
+];
+
+foreach ($matriz as $indice){
+    foreach ($indice as $valor){
+        echo "$valor ";
+    }
+    echo '<br>';
+}
+
+?>
