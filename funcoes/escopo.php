7f651567649c5781f4778730f7f876189f5f0aa5
joaorocha-web <jvrochajf@eaportal.org>
2025-04-11 13:24:49 -0300

    .

--- funcoes/escopo.php
+++ funcoes/escopo.php
@@ -7,4 +7,18 @@ function imprimirMensagens(){
 }
 
 imprimirMensagens();
+
+echo "<hr><br>";
+
+$variavel = 1;
+
+function alterarValor(){
+    $variavel = 2;
+    echo "Valor durante a função: $variavel";
+}
+
+echo "Valor antes da função: $variavel";
+alterarValor();
+echo "Valor depois da função: $variavel";
+
 ?>
