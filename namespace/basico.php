db61c1007bde8673233d04333aeb399be7cf90fd
joaorocha-web <jvrochajf@eaportal.org>
2025-04-28 13:50:02 -0300

    .

--- namespace/basico.php
+++ namespace/basico.php
@@ -4,5 +4,11 @@
 <?php
 echo __NAMESPACE__ . '<br>';
 const CONSTANTE1 = 123;
-define ('contexto\CONSTANTE2', 234);
+define ('CONSTANTE2', 234);
+define ('contexto\CONSTANTE2', 1234);
 define (__NAMESPACE__ . '\CONSTANTE3', 345);
+define ('outro_contexto\CONSTANTE4', 456);
+echo CONSTANTE1 . '<br>';
+echo CONSTANTE2 . '<br>';
+echo \CONSTANTE2 . '<br>'; // acessando a cnstante que está na raiz
+echo CONSTANTE3 . '<br>';
